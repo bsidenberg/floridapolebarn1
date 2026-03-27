df9daec341acda6edca3a0aff1cf42386c77de38
bsidenberg <info@floridapolebarn.com>
2026-03-26 21:50:04 -0400

    Fix robots.ts: remove stale /quote/thank-you disallow

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/api/', '/quote/thank-you'],
+      disallow: ['/api/'],
     },
     sitemap: 'https://floridapolebarn.com/sitemap.xml',
   }
